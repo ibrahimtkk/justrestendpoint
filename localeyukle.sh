8ef62bc212341bc37448927943970e43f6d993bf
ibrahimtkk <ibrahim.altay97@gmail.com>
2020-10-12 19:02:24 +0300

    just1

--- localeyukle.sh
+++ localeyukle.sh
@@ -4,6 +4,6 @@ echo Starting install script
 
 echo "Upload Starting"
 
-/usr/local/Cellar/atlassian-plugin-sdk/8.0.16/libexec/apache-maven-3.5.4/bin/mvn com.atlassian.maven.plugins:amps-dispatcher-maven-plugin:8.0.2:install -gs /usr/local/Cellar/atlassian-plugin-sdk/8.0.16/libexec/apache-maven-3.5.4/conf/settings.xml -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true -Dmaven.wagon.http.ssl.ignore.validity.dates=true -Dusername=ibrahim.takak -Dpassword=Zhobada35. -Datlassian.plugin.key=com.veniture.ibrahim.takak -Dserver=localhost -Dhttp.port=8089 -Dcontext.path=
+/usr/local/Cellar/atlassian-plugin-sdk/8.0.16/libexec/apache-maven-3.5.4/bin/mvn com.atlassian.maven.plugins:amps-dispatcher-maven-plugin:8.0.2:install -gs /usr/local/Cellar/atlassian-plugin-sdk/8.0.16/libexec/apache-maven-3.5.4/conf/settings.xml -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true -Dmaven.wagon.http.ssl.ignore.validity.dates=true -Dusername=ibrahim.takak -Dpassword=qwerty. -Datlassian.plugin.key=com.veniture.ibrahim.takak -Dserver=localhost -Dhttp.port=8089 -Dcontext.path=
 
 echo "Script complete"
